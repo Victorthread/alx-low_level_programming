55f7f801d02f9f46ffbc23b7c9d16cde2be58af6
Victorthread <arrayobjects@gmail.com>
2023-08-14 05:43:40 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <studio.h>
 /**
- * main- it prints out output using put
+ * main - it prints out output using put
  *
  * Return: always (0)
  *
@@ -8,5 +8,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
