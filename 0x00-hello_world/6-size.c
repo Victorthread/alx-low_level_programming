0fda102b063eedace867e51d2e043f8b93d5964d
Victorthread <arrayobjects@gmail.com>
2023-08-14 15:34:22 -0700

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -  to print data types
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu", size of(a));
+	printf("Size of an int: %lu", size of(b));
+	printf("Size of a long int: %lu", size of(c));
+	printf("Size of a long long int: %lu", size of(d));
+	printf("Size of a float: %lu", size of(e));
+}
